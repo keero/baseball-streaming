f83d09e641bb522ce52f1dab251899914992d5ce
Martin Kero <martinkero@spotify.com>
2022-05-04 19:34:29 +0200

    Add all Heat 2022 games to quick select in GUI wrapper.

--- baseball-streaming.py
+++ baseball-streaming.py
@@ -4,6 +4,38 @@ import threading
 import sys
 import os
 
+HEAT_GAMES_2022 = {
+    '2022-04-30 Elitserien Sundsvall @ Sundbyberg Game #1': {'game_id': '91830', 'series_id': '2022-elitserien-baseboll'},
+    '2022-04-30 Elitserien Sundsvall @ Sundbyberg Game #2': {'game_id': '91831', 'series_id': '2022-elitserien-baseboll'},
+    '2022-05-07 Regionserien Skellefteå @ Sundbyberg Game #1': {'game_id': '95336', 'series_id': '2022-regionserien-baseboll'},
+    '2022-05-07 Regionserien Skellefteå @ Sundbyberg Game #2': {'game_id': '95337', 'series_id': '2022-regionserien-baseboll'},
+    '2022-05-07 Elitserien Sundbyberg @ Karlskoga Game #1': {'game_id': '91839', 'series_id': '2022-elitserien-baseboll'},
+    '2022-05-07 Elitserien Sundbyberg @ Karlskoga Game #2': {'game_id': '91840', 'series_id': '2022-elitserien-baseboll'},
+    '2022-05-10 Elitserien Stockholm @ Sundbyberg': {'game_id': '91841', 'series_id': '2022-elitserien-baseboll'},
+    '2022-05-15 Regionserien Enköping @ Sundbyberg Game #1': {'game_id': '95358', 'series_id': '2022-regionserien-baseboll'},
+    '2022-05-15 Regionserien Enköping @ Sundbyberg Game #2': {'game_id': '95359', 'series_id': '2022-regionserien-baseboll'},
+    '2022-05-17 Elitserien Stockholm @ Sundbyberg': {'game_id': '91849', 'series_id': '2022-elitserien-baseboll'},
+    '2022-05-18 Regionserien Stockholm @ Sundbyberg': {'game_id': '95361', 'series_id': '2022-regionserien-baseboll'},
+    '2022-06-04 Elitserien Karlskoga @ Sundbyberg Game #1': {'game_id': '91875', 'series_id': '2022-elitserien-baseboll'},
+    '2022-06-04 Elitserien Karlskoga @ Sundbyberg Game #2': {'game_id': '91876', 'series_id': '2022-elitserien-baseboll'},
+    '2022-06-05 Juniorserien Göteborg @ Sundbyberg Game #1': {'game_id': '91758', 'series_id': '2022-juniorserien-baseboll'},
+    '2022-06-05 Juniorserien Göteborg @ Sundbyberg Game #2': {'game_id': '91759', 'series_id': '2022-juniorserien-baseboll'},
+    '2022-06-15 Juniorserien Stockholm @ Sundbyberg': {'game_id': '91764', 'series_id': '2022-juniorserien-baseboll'},
+    '2022-06-18 Elitserien Karlskoga @ Sundbyberg Game #1': {'game_id': '91883', 'series_id': '2022-elitserien-baseboll'},
+    '2022-06-18 Elitserien Karlskoga @ Sundbyberg Game #2': {'game_id': '91884', 'series_id': '2022-elitserien-baseboll'},
+    '2022-06-19 Juniorserien Karlskoga @ Sundbyberg Game #1': {'game_id': '91768', 'series_id': '2022-juniorserien-baseboll'},
+    '2022-06-19 Juniorserien Karlskoga @ Sundbyberg Game #2': {'game_id': '91769', 'series_id': '2022-juniorserien-baseboll'},
+    '2022-07-23 Elitserien Sölvesborg @ Sundbyberg Game #1': {'game_id': '91889', 'series_id': '2022-elitserien-baseboll'},
+    '2022-07-23 Elitserien Sölvesborg @ Sundbyberg Game #2': {'game_id': '91890', 'series_id': '2022-elitserien-baseboll'},
+    '2022-07-24 Juniorserien Sölvesborg @ Sundbyberg Game #1': {'game_id': '91783', 'series_id': '2022-juniorserien-baseboll'},
+    '2022-07-24 Juniorserien Sölvesborg @ Sundbyberg Game #2': {'game_id': '91784', 'series_id': '2022-juniorserien-baseboll'},
+    '2022-08-06 Regionserien Alby @ Sundbyberg Game #1': {'game_id': '95490', 'series_id': '2022-regionserien-baseboll'},
+    '2022-08-06 Regionserien Alby @ Sundbyberg Game #2': {'game_id': '95491', 'series_id': '2022-regionserien-baseboll'},
+    '2022-08-11 Juniorserien Stockholm @ Sundbyberg': {'game_id': '91798', 'series_id': '2022-juniorserien-baseboll'},
+    '2022-08-16 Elitserien Stockholm @ Sundbyberg': {'game_id': '91912', 'series_id': '2022-elitserien-baseboll'},
+    '2022-08-19 Elitserien Stockholm @ Sundbyberg': {'game_id': '91907', 'series_id': '2022-elitserien-baseboll'},
+    '2022-08-24 Regionserien Stockholm @ Sundbyberg': {'game_id': '95539', 'series_id': '2022-regionserien-baseboll'},
+}
 
 def resource_path(relative_path):
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(
@@ -30,13 +62,10 @@ sg.theme('SystemDefault')
 layout = [[sg.Text('Output directory for OBS resources', size=(30, 1), justification='right'),
            sg.Input(default_text='/home/martinkero/Documents/personal/obs/resources', size=80, key='target_dir'),
            sg.FolderBrowse()],
-          [sg.Text('Quick select series', size=(30, 1), justification='right'),
-           sg.Button(button_text='Elitserien'),
-           sg.Button(button_text='Juniorserien'),
-           sg.Button(button_text='Regionserien')],
+          [sg.Text('Select game', size=(30, 1), justification='right'),
+           sg.Combo(list(HEAT_GAMES_2022.keys()), key='game_selector', enable_events=True, readonly=True)],
           [sg.Text('', size=(30, 1), justification='right'),
-           sg.Button(button_text='Softbollserien'),
-           sg.Button(button_text='Juniorserien Softboll')],
+           sg.Text('or enter IDs manually below')],
           [sg.Text('Series ID', size=(30, 1), justification='right'),
            sg.Input(default_text='2022-elitserien-baseboll', size=30, key='series_id')],
           [sg.Text('Game ID', size=(30, 1), justification='right'),
@@ -62,6 +91,9 @@ while True:
             streamer_thread.join()
         sys.exit(0)
     event, values = window.read()
+    if event == 'game_selector':
+        window['series_id'].update(HEAT_GAMES_2022[values['game_selector']]['series_id'])
+        window['game_id'].update(HEAT_GAMES_2022[values['game_selector']]['game_id'])
     if event == 'Elitserien':
         window['series_id'].update('2022-elitserien-baseboll')
     if event == 'Juniorserien':
